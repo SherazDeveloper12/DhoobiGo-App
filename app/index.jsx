c94780b0c4243b1c2432eae44c8896e0d0aa2810
Sheraz <talhayounus065@gmail.com>
2026-02-28 23:02:11 +0500

    feat: initialize project with package.json and tailwind configuration
    - Added package.json with dependencies and scripts for the project.
    - Created tailwind.config.js for styling with NativeWind.

---
+++ app/index.jsx
@@ -0,0 +1,27 @@
+
+import { PackagePlus } from "lucide-react-native";
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View
+      className="flex h-full items-center p-8 gap-8  "
+    >
+      <View className="flex bg-blue-500 p-8 px-16   w-full items-center justify-center rounded-lg flex-row gap-4">
+      <View className="bg-blue-400 p-4 rounded-full">
+        <PackagePlus color="#F2F2F2" size={48} />
+      </View>
+      <View>
+        <Text className="text-3xl font-bold text-white">Book Your Order</Text>
+        <Text className="text-lg text-white">Ready to Place a new Order? Tap Here!</Text>
+      </View>
+      </View>
+      <View className="flex  w-full justify-center gap-4 ">
+      <View className="flex flex-row justify-between w-full ">
+        <Text className="text-xl font-bold">Your Orders</Text>
+        <Text className="text-xl font-bold text-blue-500">See All</Text>
+      </View>
+        <Text className="text-lg text-center">You Currently Have No Orders</Text>
+      </View>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+   presets: [require('nativewind/preset')],
+  theme: { extend: {} },
+  plugins: [],
+}
